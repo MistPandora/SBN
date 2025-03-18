187693a87670106f88d9396e5860f75f3a098d64
Killyan <killyan.faucon@hotmail.fr>
2025-03-18 17:26:09 +0100

    Added next-pwa for an offline mode

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
+import withPWA from 'next-pwa';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = withPWA({
+    pwa: {
+        dest: 'public',
+        disable: process.env.NODE_ENV === 'development',
+    },
     reactStrictMode: false,
-};
+});
 
 export default nextConfig;
