98acd1459f209a0d5aeb38a6a07658f593da6832
Killyan <killyan.faucon@hotmail.fr>
2025-03-18 17:57:16 +0100

    fixed pwa issues in next.config

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,6 @@ import withPWA from 'next-pwa';
 
 export default withPWA({
     dest: 'public',
-    disable: process.env.NODE_ENV === 'development',
-    register: true,
-    skipWaiting: true,
 })({
     reactStrictMode: false,
 });
