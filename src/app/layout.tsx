45f6176a08ec1307d549d798d0fb2e0ce1b1b46f
Killyan <killyan.faucon@hotmail.fr>
2025-03-18 19:13:11 +0100

    fixing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,13 @@
 import "@styles/globals.css";
+import type { Metadata } from "next";
+
+const roboto = "https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap";
+
+export const metadata: Metadata = {
+  title: "Salaire brut en net",
+  description: "Une application servant à convertir les taux brut/net",
+  applicationName: "SBN | Salaire brut en net",
+};
 
 export default function RootLayout({
   children,
@@ -7,6 +16,11 @@ export default function RootLayout({
 }>) {
   return (
     <html>
+      <head>
+        <link rel="stylesheet" href={roboto} />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="/icons/logo.png" />
+      </head>
       <body>{children}</body>
     </html>
   );
